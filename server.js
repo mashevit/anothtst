fe847f089a9551a6c3670c3294add5cb2a153ef5
mashevit <mashevit@gmail.com>
2018-11-03 15:20:16 +0200

    new for heroku

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+//Install express server
+const express = require('express');
+const path = require('path');
 
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+const app = express();
 
-app.listen(server_port, server_ip_address, function () {
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
-    console.log( "Listening on " + server_ip_address + ", server_port " + server_port  );
-
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
